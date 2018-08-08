fed8cde7c5c12a2f8c8fabd05495a6f16f6378a7
BuildTools <unconfigured@null.spigotmc.org>
2018-08-07 18:27:43 -0600

    Finished Episode 9

--- Test/Base.py
+++ Test/Base.py
@@ -1,23 +1,37 @@
 import sc2
-from sc2 import run_game, maps, Race, Difficulty
+from sc2 import run_game, maps, Race, Difficulty, position, Result
 from sc2.player import Bot, Computer
-from sc2.constants import NEXUS, PROBE, PYLON, ASSIMILATOR, GATEWAY, CYBERNETICSCORE, STALKER, STARGATE, VOIDRAY
+from sc2.constants import NEXUS, PROBE, PYLON, ASSIMILATOR, GATEWAY, CYBERNETICSCORE, STALKER, STARGATE, VOIDRAY, OBSERVER, ROBOTICSFACILITY
 import random
 import cv2
 import numpy as np
+import time
+import os
 
 TargetNexusCount = 3
 TargetGatewayCount = 5
 
 #165 iteration per minute
 
+Headless = False
+
 class SentdeBot(sc2.BotAI):
     def __init__(self):
         self.ITERATIONS_PER_MINUTE = 165
         self.MAX_WORKERS = 65
+        self.do_something_after = 0
+        self.train_data = []
+
+    def on_end(self, game_result):
+        print('--- on_end called ---')
+        print(game_result)
+
+        if game_result == Result.Victory:
+            np.save("train_data{}.mpy".format(str(int(time.time()))), np.array(self.train_data))
 
     async def on_step(self, iteration):
         self.iteration = iteration
+        await self.scout()
         await self.distribute_workers()
         await self.build_workers()
         await self.build_pylons()
@@ -26,9 +40,126 @@ class SentdeBot(sc2.BotAI):
         await self.offensive_force_buildings()
         await self.build_offensive_force()
         await self.attack()
+        await self.intel()
+
+    def random_location_variance(self, enemy_start_location):
+        x = enemy_start_location[0]
+        y = enemy_start_location[1]
+
+        x +=((random.randrange(-20,20))/100) * enemy_start_location[0]
+        y += ((random.randrange(-20, 20)) / 100) * enemy_start_location[1]
+
+        if x < 0:
+            x = 0
+        if y < 0:
+            y = 0
+        if x > self.game_info.map_size[0]:
+            x = self.game_info.map_size[0]
+        if y > self.game_info.map_size[1]:
+            y = self.game_info.map_size[1]
+
+        go_to = position.Point2(position.Pointlike((x,y)))
+        return go_to
+
+    async def scout(self):
+        if len(self.units(OBSERVER)) > 0:
+            scout = self.units(OBSERVER)[0]
+            if scout.is_idle:
+                enemy_location = self.enemy_start_locations[0]
+                move_to = self.random_location_variance(enemy_location)
+                await self.do(scout.move(move_to))
+
+        else: #If no scout build a scout
+            for rf in self.units(ROBOTICSFACILITY).ready.noqueue:
+                if self.can_afford(OBSERVER) and self.supply_left > 0:
+                    await self.do(rf.train(OBSERVER))
+
+    async def intel(self):
+        game_data = np.zeros((self.game_info.map_size[1], self.game_info.map_size[0], 3), np.uint8)#Generate base image
+
+        draw_dict = { # UnitName: [CircleSize, Color]
+            NEXUS: [15, (0, 255, 0)],
+            PYLON: [3, (20, 235, 0)],
+            PROBE: [1, (55, 200, 0)],
+
+            ASSIMILATOR: [2, (55, 200, 0)],
+            GATEWAY: [3, (200, 100, 0)],
+            CYBERNETICSCORE: [3, (150, 150, 0)],
+            STARGATE: [5, (255, 0, 0)],
+            VOIDRAY: [3, (255, 100, 0)],
+            #OBSERVER: [3, (255, 255, 255)] Drawing observer at the end ontop of enemies
+
+        }
+
+        for unit_type in draw_dict:
+            for unit in self.units(unit_type).ready:
+                nex_pos = unit.position#Get unit positions
+                cv2.circle(game_data, (int(nex_pos[0]), int(nex_pos[1])), draw_dict[unit_type][0], draw_dict[unit_type][1], -1)#Draw a circle for nexus
+
+        main_base_names = ["nexus", "commandcenter", "hatchery"]
+        for enemy_buildings in self.known_enemy_structures: #Draw buildings that arnt main base
+            pos = enemy_buildings.position
+            if enemy_buildings.name.lower() not in main_base_names:
+                cv2.circle(game_data, (int(pos[0]), int(pos[1])), 5, (200, 50, 212), -1)
+
+        for enemy_buildings in self.known_enemy_structures:#Draw main base
+            pos = enemy_buildings.position
+            if enemy_buildings.name.lower() in main_base_names:
+                cv2.circle(game_data, (int(pos[0]), int(pos[1])), 15, (0, 0, 255), -1)
+
+        for enemy_unit in self.known_enemy_units:
+
+            if not enemy_unit.is_structure:
+                worker_names = ["probe", "scv", "drone"]
+                pos = enemy_unit.position
+                if enemy_unit.name.lower() in worker_names:
+                    cv2.circle(game_data, (int(pos[0]), int(pos[1])), 1, (55, 0, 155), -1)
+                else:
+                    cv2.circle(game_data, (int(pos[0]), int(pos[1])), 1, (50, 0, 215), -1)
+
+        for obs in self.units(OBSERVER).ready:#Draw observer on top of everything
+            nex_pos = obs.position
+            cv2.circle(game_data, (int(nex_pos[0]), int(nex_pos[1])), 1, (255, 255, 255), -1)#Draw a circle for nexus
+
+        line_max = 50
+        mineral_ratio = self.minerals / 1500
+        if mineral_ratio > 1.0:
+            mineral_ratio = 1.0
+
+        vespene_ratio = self.vespene / 1500
+        if vespene_ratio > 1.0:
+            vespene_ratio = 1.0
+
+        population_ratio = self.supply_left / self.supply_cap
+        if population_ratio > 1.0:
+            population_ratio = 1.0
+
+        plausible_supply = self.supply_cap / 200.0
+
+        if((self.supply_cap - self.supply_left) != 0):
+            military_weight = len(self.units(VOIDRAY)) / (self.supply_cap - self.supply_left)
+            if military_weight > 1.0:
+                military_weight = 1.0
+        else:
+            military_weight = 0
+
+        cv2.line(game_data, (0, 19), (int(line_max * military_weight), 19), (250, 250, 200), 3)  # worker/supply ratio
+        cv2.line(game_data, (0, 15), (int(line_max * plausible_supply), 15), (220, 200, 200),
+                 3)  # plausible supply (supply/200.0)
+        cv2.line(game_data, (0, 11), (int(line_max * population_ratio), 11), (150, 150, 150),
+                 3)  # population ratio (supply_left/supply)
+        cv2.line(game_data, (0, 7), (int(line_max * vespene_ratio), 7), (210, 200, 0), 3)  # gas / 1500
+        cv2.line(game_data, (0, 3), (int(line_max * mineral_ratio), 3), (0, 255, 25), 3)  # minerals minerals/1500
+
+        self.flipped = cv2.flip(game_data, 0)#flip game data upside down because 0,0 is top left
+
+        if not Headless:
+            resized = cv2.resize(self.flipped, dsize=None, fx=2, fy=2)#size image up for display on screen
+            cv2.imshow('Intel', resized)#Diplay image
+        cv2.waitKey(1)
 
     async def build_workers(self):
-        if(len(self.units(NEXUS))*16 > len(self.units(PROBE))):
+        if(len(self.units(NEXUS))*16 > len(self.units(PROBE)) and len(self.units(NEXUS)) > 0):
             if len(self.units(PROBE)) < self.MAX_WORKERS:
                 for nexus in self.units(NEXUS).ready.noqueue:
                     if self.can_afford(PROBE):
@@ -71,6 +202,9 @@ class SentdeBot(sc2.BotAI):
                 if self.can_afford(GATEWAY) and not self.already_pending(GATEWAY):
                     await self.build(GATEWAY, near=pylon)
 
+            if self.units(CYBERNETICSCORE).ready.exists and len(self.units(ROBOTICSFACILITY)) < 1 and self.can_afford(ROBOTICSFACILITY) and not self.already_pending(ROBOTICSFACILITY):
+                await self.build(ROBOTICSFACILITY, near=pylon)
+
             if self.units(CYBERNETICSCORE).ready.exists:
                 if len(self.units(STARGATE))< (self.iteration / self.ITERATIONS_PER_MINUTE):
                     if self.can_afford(STARGATE) and not self.already_pending(STARGATE):
@@ -95,13 +229,34 @@ class SentdeBot(sc2.BotAI):
             return self.enemy_start_locations[0]
 
     async def attack(self):
-        #{UNIT: [n to fight, n to defend]}
-        agressive_units = {VOIDRAY: [8,  3]}
+        if len(self.units(VOIDRAY).idle) > 0:
+            choice = random.randrange(0,4)
 
-        for UNIT in agressive_units:
-            if self.units(UNIT).amount > agressive_units[UNIT][0] and self.units(UNIT).amount > agressive_units[UNIT][1]:
-                for s in self.units(UNIT).idle:
-                    await self.do(s.attack(self.find_target(self.state)))
+            target = False
+            if self.iteration > self.do_something_after:
+                if choice == 0:#Do nothing
+                    wait = random.randrange(20,165)
+                    self.do_something_after = self.iteration + wait
+
+                elif choice == 1:#Attack unit closest to nexus
+                    if len(self.known_enemy_units) > 0:
+                        target = self.known_enemy_units.closest_to(random.choice(self.units(NEXUS)))
+
+                elif choice == 2:#Attack enemy start locaton
+                    if len(self.known_enemy_structures) > 0:
+                        target = random.choice(self.known_enemy_structures)
+
+                elif choice == 3:#Attack enemy start location
+                    target = self.enemy_start_locations[0]
+
+                if target:
+                    for vr in self.units(VOIDRAY).idle:
+                        await self.do(vr.attack(target))
+
+                y = np.zeros(4)
+                y[choice]=1
+                print(y)
+                self.train_data.append([y, self.flipped])
 
 run_game(maps.get("AbyssalReefLE"), [
     Bot(Race.Protoss, SentdeBot()),
